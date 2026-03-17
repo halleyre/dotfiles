0b5c06cd5b842b541c7f4e31fe83268b231e163c
halleyre <magpie2@halleyre.dev>
2026-03-17 12:08:39 +0000

    welcome brolga

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -1,11 +1,23 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+  };
+  outputs = { self, nixpkgs, nixos-wsl }: {
     nixosConfigurations.magpie = nixpkgs.lib.nixosSystem {
       modules = [ ./magpie/configuration.nix ];
     };
     nixosConfigurations.harrier = nixpkgs.lib.nixosSystem {
       modules = [ ./harrier/configuration.nix ];
+    };
+    nixosConfigurations.brolga = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [ ./brolga/configuration.nix
+        nixos-wsl.nixosModules.default {
+          system.stateVersion = "24.11";
+          wsl.enable = true;
+        }
+      ];
     };
   };
 }
